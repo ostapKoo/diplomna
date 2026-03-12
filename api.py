83dd3cde35faccda7ebc04eca036043e2b06ef94
Oleg Ostapenko <o.ostapenko@apptimized.com>
2026-03-12 15:31:45 +0200

    додано логування HTTP трафіку та кастомний обробник 404 помилки

--- api.py
+++ api.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
+from starlette.exceptions import HTTPException as StarletteHTTPException
 import uuid
+import time
 from pydantic import BaseModel
 from logger_config import logger, trace_id_var
 
@@ -14,26 +16,50 @@ class CommandRequest(BaseModel):
 
 
 @app.middleware("http")
-async def add_trace_id_middleware(request: Request, call_next):
+async def log_requests_and_trace(request: Request, call_next):
+
     trace_id = str(uuid.uuid4())[:8]
     trace_id_var.set(trace_id)
 
+
+    start_time = time.time()
+
+
     response = await call_next(request)
 
+
+    process_time = (time.time() - start_time) * 1000
+
+
+    logger.info(
+        f"HTTP {request.method} {request.url.path} | Status: {response.status_code} | Time: {process_time:.2f}ms",
+        extra={'context': 'HTTP_Traffic'}
+    )
 
     response.headers["X-Trace-ID"] = trace_id
     return response
 
 
 
+@app.exception_handler(StarletteHTTPException)
+async def custom_404_handler(request: Request, exc: StarletteHTTPException):
+    if exc.status_code == 404:
+        logger.warning(f"Спроба доступу до неіснуючої сторінки: {request.url.path}", extra={'context': '404_Error'})
+        return JSONResponse(
+            status_code=404,
+            content={
+                "error": "Сторінку не знайдено",
+                "message": "Халепа! Такого маршруту не існує. Перевірте адресу URL."
+            }
+        )
+    return JSONResponse(status_code=exc.status_code, content={"detail": exc.detail})
+
+
+
 @app.exception_handler(Exception)
 async def global_exception_handler(request: Request, exc: Exception):
     current_trace_id = trace_id_var.get()
-
-
     logger.error(f"Необроблена помилка API: {str(exc)}", exc_info=True, extra={'context': 'API_Error'})
-
-
     return JSONResponse(
         status_code=500,
         content={
@@ -48,7 +74,6 @@ async def global_exception_handler(request: Request, exc: Exception):
 async def execute_command(req: CommandRequest):
     logger.info(f"Отримано команду: '{req.command}' для {req.target}", extra={'context': 'UserRequest'})
 
-    # Тригер для тестування Sentry
     if req.command.lower() == "зламайся":
         raise ValueError("Критичний збій бази даних! (Тест для Sentry)")
 
